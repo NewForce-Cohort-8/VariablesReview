e4cab294efc3200734a85ab016129f8c7b3f496b
Tommy-Spurlock <tspurloc@gmail.com>
2023-08-30 16:01:50 -0400

    completed first review

---
+++ main.js
@@ -0,0 +1,83 @@
+// Variables 
+
+var myName = "Tommy"  //string
+
+const age = 35       //integer AKA int
+
+let likesSleep = true //boolean AKA bool
+
+
+// keywords var, const, let
+
+
+const MiaName = "Mia"
+
+    // MiaName = "Dottie"     // cannot reassign a const variable
+
+
+    // console.log(MiaName)
+
+
+
+    let kittyName = ""
+
+        // console.log(kittyName)
+
+        kittyName = "Nona"
+
+        // console.log(kittyName)
+
+        kittyName = "Avacado"
+        // console.log(kittyName)
+
+// Objects and Arrays
+
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 87, 23, 19]
+
+const words = ["the", "wolf", "howled"]
+
+const bools = [true, false, false, true]
+
+const anything = [true, "hello", 19287236]
+
+// console.log(words.length)
+
+// console.log(words[0])
+
+// console.log(numbers[7])
+
+
+const sarahObject = {
+    firstName: "Sarah",
+    lastName: "Brooks",
+    workPlace: "NewForce",
+    likesToRead: true,
+    chaosLevel: 4,
+    favebooks: ["cat in the hat", "curriculum", "Cresent City"],
+    barry: {
+        firstName: "Barry",
+        moniker: "the Kid",
+        lastName: "Griffith",
+        barrysBooks: ["cat in the hat", "curriculum", "Cresent City"],
+    }
+}
+
+//use dot notation or bracket notation to get values out of object 
+
+
+console.log(sarahObject.barry.barrysBooks[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
